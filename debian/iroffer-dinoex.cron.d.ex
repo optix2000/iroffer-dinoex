671d50c5f48c25fef1609f911e38ecbd8b56a88c
Weilu Jia <optix2000@teitoku.net>
2016-05-20 05:35:35 +0000

    Remove example files

--- debian/iroffer-dinoex.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the iroffer-dinoex package
-#
-0 4	* * *	root	[ -x /usr/bin/iroffer-dinoex_maintenance ] && /usr/bin/iroffer-dinoex_maintenance
--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(iroffer-dinoex):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="iroffer-dinoex" command="/usr/bin/iroffer-dinoex"
